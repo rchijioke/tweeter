d58e419f61aca65af38a3f158485c196f2c392ff
Roy <ekeneroy@gmail.com>
2023-05-04 13:56:35 -0500

    used jQuery.text to render an error message

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -35,30 +35,6 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
 
   const renderTweets = function (tweets) {
     const $tweetsContainer = $(".tweets-container");
@@ -67,7 +43,7 @@ $(document).ready(function () {
     for (const tweet of tweets) {
       // calls createTweetElement for each tweet
       const $tweet = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
+      // takes return value and prepends it to the tweets container
       $tweetsContainer.prepend($tweet);
     }
   };
@@ -82,7 +58,9 @@ $(document).ready(function () {
         renderTweets(data);
       },
       error: function (error) {
-        console.log("Error fetching tweets: ", error);
+        const errorMessage = "Error fetching tweets: " + error.statusText;
+        $('#error-messages').text(errorMessage);
+        return;
       },
     });
   };
